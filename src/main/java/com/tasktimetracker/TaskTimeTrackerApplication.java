a30bd89d506077a175b2eb7a26d3e6489dc12524
ndtm1 <degtyarevn5@gmail.com>
2026-04-15 12:30:20 +0300

    set up

---
+++ src/main/java/com/tasktimetracker/TaskTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tasktimetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTimeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasktimetracker/TaskTimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasktimetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
