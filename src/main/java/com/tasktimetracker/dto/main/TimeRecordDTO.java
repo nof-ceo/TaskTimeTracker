f9dbd53a21e00d85e7b42c947dd1cd5c74f1201d
ndtm1 <degtyarevn5@gmail.com>
2026-04-18 17:43:10 +0300

    feat: DTO для затраченного времени

---
+++ src/main/java/com/tasktimetracker/dto/main/TimeRecordDTO.java
@@ -0,0 +1,23 @@
+package com.tasktimetracker.dto.main;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TimeRecordDTO (
+    @Schema(description = "ID работника", example = "30928we-23few34-34rf34")
+    UUID employeeId,
+
+    @Schema(description = "ID задачи", example = "30928we-23few34-34rf34")
+    TaskDTO taskId,
+
+    @Schema(description = "Время начала", example = "")
+    LocalDateTime startTime,
+
+    @Schema(description = "Время конца", example = "")
+    LocalDateTime endTime,
+
+    @Schema(description = "Описание", example = "")
+    String description
+) {}
